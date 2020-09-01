61ff2c0e34c2dd7c5dd5c28df1daab22de1ae009
Aleksandr Cherevko <cherevko.alexandr.1997@gmail.com>
2020-09-01 13:10:39 +0300

    fix task3

--- src/chrvk/hw3/task3/Main.java
+++ src/chrvk/hw3/task3/Main.java
@@ -1,22 +1,30 @@
 package chrvk.hw3.task3;
 
-//Дана строк состоящая из любых символов.
-//      Не используя стандартный метод String.toLowerCase()
-//      перевести все большие латинские буквы в маленькие.
-//      Пример:
-//      Input: "#1AmC"
-//      Output: "#1amc"
+/*
+Дана строк состоящая из любых символов.
+Не используя стандартный метод String.toLowerCase()
+перевести все большие латинские буквы в маленькие.
+Пример:
+Input: "#1AmC"
+Output: "#1amc"
+ */
 
 public class Main {
     public static void main(String[] args) {
         String line = "#1AmC";
-        String alphabet1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String alphabet2 = "abcdefghijklmnopqrstuvwxyz";
+        System.out.println(line);
+        System.out.println(toLowerCase(line));
+    }
 
-        System.out.println(line);
-        for (int j = 0; j < alphabet1.length(); j++) {
-            line = line.replace(alphabet1.charAt(j), alphabet2.charAt(j));
+    public static String toLowerCase(String line) {
+        char[] chars = line.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            if ((int) chars[i] >= 'A' && (int) chars[i] <= 'Z') {
+                int diff = 'z' - 'Z';
+                chars[i] += diff;
+            }
         }
-        System.out.println(line);
+        return new String(chars);
     }
 }
